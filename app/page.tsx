3fe05539cacfaaa0f3a67124be1c3f09e3df84c7
rafa <rafa@MacBookPro.home>
2026-02-03 14:45:01 +0000

    Remove unused anime import

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useRef, useState } from "react";
 import { BlurGradientBg } from "@/jsm/BlurGradientBg.module.js";
 import { motion } from "framer-motion";
-import * as anime from "animejs";
-
 
 export default function Home() {
   const cursorRef = useRef<HTMLDivElement>(null);
